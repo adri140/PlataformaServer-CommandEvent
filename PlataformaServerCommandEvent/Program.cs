96165c80c17487f8ce821b3908812330036e0dcf
adrian recio <adrianrecio17@gmail.com>
2019-03-11 09:43:12 +0100

    Agregar archivos de proyecto.

---
+++ PlataformaServerCommandEvent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlataformaServerCommandEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
